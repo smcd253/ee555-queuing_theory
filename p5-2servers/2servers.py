79069991e2154de6b6cbe17b61ffd58b1b6cf30c
Spencer McDonough <shmcdono@usc.edu>
2020-02-21 16:04:02 -0800

    framework for q5

---
+++ p5-2servers/2servers.py
@@ -0,0 +1,12 @@
+import xlsxwriter   # write results to excel workbook
+import sys          # take in command line arguments
+import math         # factorial
+
+# inputs
+u1 = float(sys.argv[1])         # arrival rate
+u2 = float(sys.argv[2])         # service rate
+l = float(sys.argv[3])          # l
+
+
+# calculate p(0,0)
+
