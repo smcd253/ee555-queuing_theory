37807385668da34f8629b692b93df8900aa8c55f
Spencer McDonough <shmcdono@usc.edu>
2020-02-21 16:48:29 -0800

    finish q5

--- p5-2servers/2servers.py
+++ p5-2servers/2servers.py
@@ -7,6 +7,14 @@ u1 = float(sys.argv[1])         # arrival rate
 u2 = float(sys.argv[2])         # service rate
 l = float(sys.argv[3])          # l
 
+if l < u1 and u1 > u2:
+    # calculate p(0,0)
+    a = float(l / (u1/l + u1 * u2))
+    inf_geo_sum = float((l / u1) / (1 - l / u1))
+    
+    p00 = float(math.pow(1 + a * u1 + a / l + a * inf_geo_sum, -1))
 
-# calculate p(0,0)
-
+    print("p(idle) = ", p00)
+    print("utulization = ", 1 - p00)
+else:
+    print("invalid parameters")
